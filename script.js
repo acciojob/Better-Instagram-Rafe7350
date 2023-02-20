d0f0d95810a0a6de546bdc432a1d2ced6a3b47f4
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-02-20 12:43:34 +0530

    add

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
 //your code here
 window.onload= function(){
     var image=document.querySelectorAll('.image');
-    var 
-}
+    var boxes=document.querySelectorAll('.boxes');
+
+    image.addEventListener('dragstart',(e)=>{
+        console.log("dragstart");
+        
+        e.target.className+='.selected';
+        setTimeout(()=>{
+            e.target.className+='hide';
+        },0);
+       
+    });
+
+    image.addEventListener('dragend',()=>{
+        console.log("dragend");
+    });
+
+    boxes.forEach(box=>{
+        box.addEventListener('dragover',(e)=>{
+           e.preventDefault();
+        });
+        box.addEventListener('dragenter',()=>{
+
+        });
+        box.addEventListener('dragleave',()=>{
+
+        });
+        box.addEventListener('drop',()=>{
+
+        });
+    
+
+        })
+    
+
+
+
 
 
